3d7e9f6746dd4030755848d35db088ca2cc206af
Hugo Fernández <hugoemprendiendo@gmail.com>
2025-12-28 15:23:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2064210776-ab0d4",
+  "appId": "1:1086224270184:web:545529aa036cb0168d8cdd",
+  "apiKey": "AIzaSyB9OhFmxVDKeImFNjCYTpTATSBUstuBN-U",
+  "authDomain": "studio-2064210776-ab0d4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1086224270184"
+};
